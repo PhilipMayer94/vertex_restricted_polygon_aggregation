d021995f1ac4742277e02cd6db2a307575aa6b19
P. Mayer <anon@alenex2024.com>
2024-11-11 09:08:26 +0100

    mssing files

---
+++ include/config.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+
+#define PROJECT_ROOT std::string("/home/philip/Projects/polygon_aggregation_via_ilp/")
+#define DATA_ROOT std::string("/home/philip/Projects/polygon_aggregation_via_ilp/data/")
